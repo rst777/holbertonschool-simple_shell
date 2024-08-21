c063ce739ccb8962eb61c16d08e18ae3431efa4a
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-21 10:01:49 +0200

    retour a la version precedente

--- exit_command.c
+++ exit_command.c
@@ -5,34 +5,9 @@
  */
 void check_exit_command(char *buffer)
 {
-	char *command = buffer; /**Début de la chaîne de commande */
-	int exit_code = 0;		/** Code de sortie par défaut */
-
-	/** Supprimer les espaces en début */
-	while (*command == ' ')
+	if (strcmp(buffer, "exit") == 0)
 	{
-		command++;
-	}
-	/** Supprimer les espaces en fin */
-	char *end = command + strlen(command) - 1;
-	while (end > command && *end == ' ')
-	{
-		end--;
-	}
-	/** Terminer la chaîne */
-	end[1] = '\0';
-
-	/** Vérifier si la commande est "exit" */
-	if (strncmp(command, "exit", 4) == 0 && (command[4] == '\0' || command[4] == ' '))
-	{
-		/** Lire et utiliser le code de sortie s'il est présent */
-		if (command[4] != '\0')
-		{
-			exit_code = atoi(command + 4);
-		}
-
-		/** Libérer la mémoire et quitter */
 		free(buffer);
-		exit(exit_code);
+		exit(0);
 	}
 }
