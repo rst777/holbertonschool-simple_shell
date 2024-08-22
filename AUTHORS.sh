e55300f7c798e45e28dec560178d43352532694e
Your Name <you@example.com>
2024-08-22 17:25:01 +0200

    AUTHORS.sh

---
+++ AUTHORS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Auteur: Lamine M B, R Stéphane
+# Date: 22 août 2024
+# Description: Ce script affiche un message de bienvenue et le nom de l'utilisateur actuel.
+
+# Afficher un message de bienvenue
+echo "Bonjour, Axel ! Bienvenue dans le monde du shell scripting."
